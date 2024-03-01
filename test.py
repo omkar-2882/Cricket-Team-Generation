29ed2ffb55276117e0ac9c7ebd5859f70c5a536c
Omkar Gawde <omkar.gawde2002@gmail.com>
2024-03-01 19:37:36 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import time
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+
+# Setting up the Chrome driver
+chrome_driver_path = "D:\Downloads\chromedriver-win64\chromedriver-win64"
+webdriver_path = "{Your Webdriver path}\Chromedriver"
+
+driver = webdriver.Chrome(chrome_driver_path)
+
+
