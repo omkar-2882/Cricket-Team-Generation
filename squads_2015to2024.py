22ae53720dfc6aad14018ba643aaa3453377520c
Omkar Gawde <omkar.gawde2002@gmail.com>
2024-03-28 14:46:02 +0530

    created squads object

---
+++ squads_2015to2024.py
@@ -0,0 +1,80 @@
+ipl_mapping = {
+    2015: {
+        'Mumbai Indians': ['Rohit Sharma', 'Aiden Blizzard', 'Unmukt Chand', 'Alex Hales', 'Siddhesh Lad', 'Colin Munro', 'Parthiv Patel', 'Nitish Rana', 'Ambati Rayudu', 'Lendl Simmons', 'Aditya Tare', 'Aaron Finch', 'Corey Anderson', 'Shreyas Gopal', 'Hardik Pandya', 'Kieron Pollard', 'Jasprit Bumrah', 'Marchant de Lange', 'Harbhajan Singh', 'Ben Hilfenhaus', 'Lasith Malinga', 'Mitchell McClenaghan', 'Abhimanyu Mithun', 'Pragyan Ojha', 'Jagadeesha Suchith', 'Pawan Suyal', 'Vinay Kumar', 'Akshay Wakhare'],
+        'Chennai Super Kings': ['MS Dhoni', 'Ankush Bains', 'Faf du Plessis', 'ER Dwivedi', 'Michael Hussey', 'Brendon McCullum', 'Suresh Raina', 'Baba Aparajith', 'Ravichandran Ashwin', 'Dwayne Bravo', 'Ravindra Jadeja', 'Mithun Manhas', 'Irfan Pathan', 'Dwayne Smith', 'Kyle Abbott', 'Samuel Badree', 'Matt Henry', 'Ronit More', 'Pawan Negi', 'Ashish Nehra', 'Ishwar Pandey', 'Pratyush Singh', 'Mohit Sharma', 'Rahul Sharma', 'Andrew Tye'],
+        'Delhi Daredevils': ['Mayank Agarwal', 'Srikar Bharat', 'Quinton de Kock', 'Chidhambaram Gautam', 'Travis Head', 'Shreyas Iyer', 'Manoj Tiwary', 'Saurabh Tiwary', 'Yuvraj Singh', 'Jean-Paul Duminy', 'Nathan Coulter-Nile', 'Kedar Jadhav', 'Angelo Mathews', 'Albie Morkel', 'Marcus Stoinis', 'Imran Tahir', 'KK Jiyas', 'Zaheer Khan', 'Amit Mishra', 'Mohammed Shami', 'Shahbaz Nadeem', 'Gurinder Sandhu', 'Jaydev Unadkat', 'Jayant Yadav', 'Domnic Muthuswami'],
+        'Kings XI Punjab': ['George Bailey', 'Gurkeerat Singh Mann', 'Shaun Marsh', 'David Miller', 'Nikhil Naik', 'Wriddhiman Saha', 'Virender Sehwag', 'Murali Vijay', 'Manan Vohra', 'Rishi Dhawan', 'Glenn Maxwell', 'Axar Patel', 'Thisara Perera', 'Anureet Singh', 'Parvinder Awana', 'Beuran Hendricks', 'Mitchell Johnson', 'Sandeep Sharma', 'Shardul Thakur', 'Yogesh Golwalkar', 'Karanveer Singh', 'Shivam Sharma', 'Domnic Muthuswami'],
+        'Kolkata Knight Riders': ['Gautam Gambhir', 'Sheldon Jackson', 'Manish Pandey', 'Vaibhav Rawal', 'Robin Uthappa', 'Suryakumar Yadav', 'Chris Lynn', 'Azhar Mahmood', 'Johan Botha', 'Piyush Chawla', 'Brad Hogg', 'Sunil Narine', 'Sumit Narwal', 'Yusuf Pathan', 'Andre Russell', 'Shakib Al Hasan', 'Ryan ten Doeschate', 'James Neesham', 'KC Cariappa', 'Pat Cummins', 'Kuldeep Yadav', 'Morne Morkel', 'Umesh Yadav', 'Aditya Garhwal', 'Veer Pratap Singh'],
+        'Rajasthan Royals': ['Karun Nair', 'Ajinkya Rahane', 'Sanju Samson', 'Steven Smith', 'Dishant Yagnik', 'Shane Watson', 'Rajat Bhatia', 'Stuart Binny', 'Ben Cutting', 'James Faulkner', 'Deepak Hooda', 'Chris Morris', 'Abhishek Nayar', 'Pardeep Sahu', 'Rahul Tewatia', 'Ankit Sharma', 'Dhawal Kulkarni', 'Vikramjeet Singh Malik', 'Dinesh Salunkhe', 'Tim Southee', 'Barinder Sran', 'Pravin Tambe', 'Rusty Theron', 'Kane Richardson', 'Sagar Trivedi'],
+        'Royal Challengers Bangalore': ['Virat Kohli', 'S Badrinath', 'SA Bhavane', 'Manvinder Bisla', 'AB de Villiers', 'Dinesh Karthik', 'Sarfaraz Khan', 'Nic Maddinson', 'Rilee Rossouw', 'Sean Abbott', 'Chris Gayle', 'Iqbal Abdullah', 'Jalaj Saxena', 'Mandeep Singh', 'Daren Sammy', 'David Wiese', 'Varun Aaron', 'Abu Nechim Ahmed', 'S Aravind', 'Yuzvendra Chahal', 'Ashok Dinda', 'Harshal Patel', 'Sandeep Warrier', 'Mitchell Starc', 'Adam Milne', 'Yogesh Takawale', 'Vijay Zol'],
+        'Sunrisers Hyderabad': ['David Warner', 'Ricky Bhui', 'Shikhar Dhawan', 'Eoin Morgan', 'Naman Ojha', 'Kevin Pietersen', 'KL Rahul', 'Kane Williamson', 'Ashish Reddy', 'Bipul Sharma', 'Ravi Bopara', 'Moises Henriques', 'Parvez Rasool', 'P Prasanth', 'Laxmi Ratan Shukla', 'Hanuma Vihari', 'Trent Boult', 'Siddarth Kaul', 'Praveen Kumar', 'Bhuvneshwar Kumar', 'Chama Milind', 'Karn Sharma', 'Ishant Sharma', 'Dale Steyn']
+    },
+
+    2016: {
+        'Delhi Daredevils': [
+            'Mayank Agarwal', 'Sam Billings', 'Quinton de Kock', 'Akhil Herwadkar', 
+            'Shreyas Iyer', 'Karun Nair', 'Rishabh Pant', 'Sanju Samson', 
+            'Carlos Brathwaite', 'Nathan Coulter-Nile', 'Jean-Paul Duminy', 
+            'Mahipal Lomror', 'Chris Morris', 'Zaheer Khan', 'Khaleel Ahmed', 
+            'Imran Tahir', 'Chama Milind', 'Amit Mishra', 'Mohammed Shami', 
+            'Shahbaz Nadeem', 'Pawan Negi', 'Pratyush Singh', 'Pawan Suyal', 
+            'Jayant Yadav'
+        ],
+        'Gujarat Lions': [
+            'Suresh Raina', 'Paras Dogra', 'Eklavya Dwivedi', 'Aaron Finch', 
+            'Ishan Kishan', 'Dinesh Karthik', 'Brendon McCullum', 'Akshdeep Nath', 
+            'Jaydev Shah', 'Umang Sharma', 'Dwayne Bravo', 'James Faulkner', 
+            'Ravindra Jadeja', 'Dwayne Smith', 'Amit Mishra', 'Shadab Jakati', 
+            'Shivil Kaushik', 'Dhawal Kulkarni', 'Praveen Kumar', 'Sarabjit Ladda', 
+            'Pradeep Sangwan', 'Dale Steyn', 'Pravin Tambe', 'Andrew Tye'
+        ],
+        'Kings XI Punjab': [
+            'Murali Vijay', 'Hashim Amla', 'Armaan Jaffer', 'Gurkeerat Singh Mann', 
+            'David Miller', 'Nikhil Naik', 'Wriddhiman Saha', 'Manan Vohra', 
+            'Farhaan Behardien', 'Rishi Dhawan', 'Axar Patel', 'Pardeep Sahu', 
+            'Kyle Abbott', 'Anureet Singh', 'KC Cariappa', 'Mitchell Johnson', 
+            'Sandeep Sharma', 'Mohit Sharma', 'Swapnil Singh', 'Shardul Thakur'
+        ],
+        'Kolkata Knight Riders': [
+            'Gautam Gambhir', 'Sheldon Jackson', 'Chris Lynn', 'Colin Munro', 
+            'Manish Pandey', 'Robin Uthappa', 'Suryakumar Yadav', 'Piyush Chawla', 
+            'Brad Hogg', 'Jason Holder', 'Manan Sharma', 'Sunil Narine', 'Yusuf Pathan', 
+            'Andre Russell', 'Rajagopal Sathish', 'Shakib Al Hasan', 'Jagadeesha Suchith', 
+            'Jaydev Unadkat', 'Umesh Yadav'
+        ],
+        'Mumbai Indians': [
+            'Rohit Sharma', 'Jos Buttler', 'Unmukt Chand', 'Martin Guptill', 
+            'Siddhesh Lad', 'Parthiv Patel', 'Nitish Rana', 'Ambati Rayudu', 
+            'Jitesh Sharma', 'Corey Anderson', 'Shreyas Gopal', 'Kishore Kamath', 
+            'Krunal Pandya', 'Hardik Pandya', 'Kieron Pollard', 'Jasprit Bumrah', 
+            'Marchant de Lange', 'Harbhajan Singh', 'Mitchell McClenaghan', 
+            'Deepak Punia', 'Nathu Singh', 'Tim Southee', 'Jagadeesha Suchith', 
+            'Jerome Taylor', 'Vinay Kumar', 'Akshay Wakhare'
+        ],
+        'Rising Pune Supergiants': [
+            'MS Dhoni', 'George Bailey', 'Ankush Bains', 'Peter Handscomb', 
+            'Usman Khawaja', 'Ajinkya Rahane', 'Saurabh Tiwary', 'Baba Aparajith', 
+            'Ravichandran Ashwin', 'Rajat Bhatia', 'Albie Morkel', 'Irfan Pathan', 
+            'Thisara Perera', 'Murugan Ashwin', 'Bowler', 'Scott Boland', 
+            'Deepak Chahar', 'Ashok Dinda', 'Jaskaran Singh', 'Ishwar Pandey', 
+            'Ishant Sharma', 'RP Singh', 'Adam Zampa'
+        ],
+        
+        'Royal Challengers Bangalore': [
+            'Virat Kohli', 'AB de Villiers', 'Travis Head', 'Sarfaraz Khan', 'KL Rahul', 
+            'Sachin Baby', 'Stuart Binny', 'Chris Gayle', 'Iqbal Abdulla', 'Kedar Jadhav', 
+            'Mandeep Singh', 'Parvez Rasool', 'Shane Watson', 'David Wiese', 'Varun Aaron', 
+            'Abu Nechim', 'Sreenath Aravind', 'Yuzvendra Chahal', 'Praveen Dubey', 'Chris Jordan', 
+            'Akshay Karnewar', 'Vikramjeet Malik', 'Harshal Patel', 'Kane Richardson', 
+            'Tabraiz Shamsi', 'Vikas Tokas'
+        ],
+        
+        'Sunrisers Hyderabad': [
+            'David Warner', 'Ricky Bhui', 'Shikhar Dhawan', 'Eoin Morgan', 'Naman Ojha', 
+            'Tirumalasetti Suman', 'Aditya Tare', 'Kane Williamson', 'Yuvraj Singh', 
+            'Ashish Reddy', 'Bipul Sharma', 'Ben Cutting', 'Moises Henriques', 'Deepak Hooda', 
+            'Vijay Shankar', 'Trent Boult', 'Siddarth Kaul', 'Bhuvneshwar Kumar', 
+            'Abhimanyu Mithun', 'Mustafizur Rahman', 'Karn Sharma', 'Barinder Sran'
+        ]
+    }
+}
